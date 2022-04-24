b945ffeb1db04ace5d5994022b912ac9966681c6
Andrew Voss <andrewvoss212@gmail.com>
2022-04-24 16:18:18 -0500

    Create copy constructor for `List`

--- src/container.hpp
+++ src/container.hpp
@@ -18,8 +18,18 @@ class List {
         I length;
     public:
         List() : start(nullptr), end(nullptr), length(0) {}
+        List(const List<T, I>& list) { *this = list.clone() }
         List(T* arr, I len) {
             for (I i = 0; i < len; ++i) this->push_back(arr[i]);
+        }
+        List<T, I> clone() const {
+            List<T, I> list;
+            ListNode<T>* ptr;
+
+            for (ptr = start; ptr != nullptr; ptr = ptr->next)
+                list.push_back(ptr->value);
+            
+            return list;
         }
         void push_back(const T& t) {
             if (start == nullptr) {
@@ -101,7 +111,7 @@ class List {
 
             return ptr->value;
         }
-        I len() const { return this->length; }
+        inline I len() const { return this->length; }
 };
 
 struct Range {
