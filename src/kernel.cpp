7779a639c5c773864a79385cf9a84c246b0b2095
Andrew Voss <andrewvoss212@gmail.com>
2022-04-24 18:24:10 -0500

    Add welcome message

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -4,5 +4,11 @@
 #include "shell.hpp"
 
 extern "C" void kernel_main(u16 kernel_sectors) {
+    puts(
+R"(Welcome to <insert OS name here>!
+Type `help` for a list of commands.
+)",
+        0x0a
+    );
     start_shell();
 }
